f929cc4fece09c0c05e4eaf8777f02b4bd222ca9
kiryuhakipyatok <goodapple5207@gmail.com>
2025-05-18 17:57:14 +0300

    auth fix

--- internal/domain/repositories/user-repositoty.go
+++ internal/domain/repositories/user-repositoty.go
@@ -77,7 +77,7 @@ func (ur *userRepository) ExistByLoginOrTg(ctx context.Context, login, tg string
 
 func (ur *userRepository) FindBy(ctx context.Context,vari,val string) (*entities.User, error){
 		user:=entities.User{}
-		query:=fmt.Sprintf("SELECT id,login,telegram,chat_id,rating,total_rating,number_of_ratings,games,avatar,discord,date_of_register from users where %s = $1",vari)
+		query:=fmt.Sprintf("SELECT id,login,telegram,chat_id,rating,total_rating,number_of_ratings,games,password,avatar,discord,date_of_register from users where %s = $1",vari)
 		if err := ur.DB.QueryRow(ctx,query,val).Scan(
 			&user.Id,
 			&user.Login,
@@ -87,6 +87,7 @@ func (ur *userRepository) FindBy(ctx context.Context,vari,val string) (*entities
 			&user.TotalRating,
 			&user.NumberOfRatings,
 			&user.Games,
+			&user.Password,
 			&user.Avatar,
 			&user.Discord,
 			&user.DateOfRegister,
